6eba57a467733e85c6eaad40d3b6c7f1865593be
GaoxiangChen <chengaoxiang@chengaoxiangdeMacBook-Pro.local>
2017-05-17 14:17:49 +0800

    1

---
+++ Queen.java
@@ -0,0 +1,53 @@
+package queen;
+
+import java.util.Scanner;
+
+public class Queen {
+			
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Queen qn = new Queen(n);
+		qn.arrayOut(0);
+		System.out.println(qn.arrayNum);
+	}
+	
+	int n,arrayNum = 0;
+	int[] x;
+	
+	public Queen(int n){
+			this.n = n;
+			this.arrayNum = 0;
+			x = new int[n];			
+	}
+
+	
+	public void arrayOut(int i){
+		if(i == n){
+			output();
+			arrayNum ++;
+			return;
+		}
+		for(int j = 0;j < n;j ++){
+			x[i] = j;
+			if(isRight(i)) arrayOut(i + 1);
+			else x[i] = 0;
+	}
+	}
+	
+	public boolean isRight(int i){
+		for(int j = 0;j < i;j ++){
+			if(x[j] == x[i] || (Math.abs(x[j] - x[i]) == Math.abs(j - i))){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void output(){
+		for(int i = 0;i < n;i ++){
+			System.out.print(x[i]);
+		}
+		System.out.println();
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf820
+{\fonttbl\f0\fnil\fcharset134 PingFangSC-Regular;}
+{\colortbl;\red255\green255\blue255;\red29\green29\blue29;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c14902\c14902\c14902;\cssrgb\c100000\c100000\c100000;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+1}
